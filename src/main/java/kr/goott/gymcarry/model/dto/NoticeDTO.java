7f58379b86598ad77e9f2469d3ea4e0cd27411a1
hwikang <yslg86@gmail.com>
2019-04-16 18:22:23 +0900

    notice

---
+++ src/main/java/kr/goott/gymcarry/model/dto/NoticeDTO.java
@@ -0,0 +1,51 @@
+package kr.goott.gymcarry.model.dto;
+
+public class NoticeDTO {
+	private int noticeno;
+	private String title;
+	private String des;
+	private int views; 
+	private String regdate;
+	private String noticeImg;
+	
+	public NoticeDTO() {};
+	
+	public int getNoticeno() {
+		return noticeno;
+	}
+	public void setNoticeno(int noticeno) {
+		this.noticeno = noticeno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDes() {
+		return des;
+	}
+	public void setDes(String des) {
+		this.des = des;
+	}
+	public int getViews() {
+		return views;
+	}
+	public void setViews(int views) {
+		this.views = views;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
+	public String getNoticeImg() {
+		return noticeImg;
+	}
+
+	public void setNoticeImg(String noticeImg) {
+		this.noticeImg = noticeImg;
+	}
+}
