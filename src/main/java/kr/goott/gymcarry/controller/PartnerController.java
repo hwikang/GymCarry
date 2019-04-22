03f30835f0190e9f2bf1d2e06b34d01aedc92944
hwikang <yslg86@gmail.com>
2019-04-22 10:13:04 +0900

    d

--- src/main/java/kr/goott/gymcarry/controller/PartnerController.java
+++ src/main/java/kr/goott/gymcarry/controller/PartnerController.java
@@ -1,28 +1,24 @@
-package kr.goott.gymcarry.controller;
-
-import java.util.List;
-
-import javax.inject.Inject;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import kr.goott.gymcarry.model.dao.PartnerDAO;
-import kr.goott.gymcarry.model.dto.UserDTO;
-
-@Controller
-public class PartnerController {
-	
-	@Inject
-	PartnerDAO partnerDAO;
-	
-	@GetMapping(value="partner.do")
-	public ModelAndView viewPartner(ModelAndView mav) {
-		mav.setViewName("partner/partnerList");
-		List<UserDTO> list = partnerDAO.getAllUser();
-		
-		mav.addObject("list",list);
-		return mav;
-	}
-}
+/*
+ * package kr.goott.gymcarry.controller;
+ * 
+ * import java.util.List;
+ * 
+ * import javax.inject.Inject;
+ * 
+ * import org.springframework.stereotype.Controller; import
+ * org.springframework.web.bind.annotation.GetMapping; import
+ * org.springframework.web.servlet.ModelAndView;
+ * 
+ * import kr.goott.gymcarry.model.dao.PartnerDAO; import
+ * kr.goott.gymcarry.model.dto.UserDTO;
+ * 
+ * @Controller public class PartnerController {
+ * 
+ * @Inject PartnerDAO partnerDAO;
+ * 
+ * @GetMapping(value="partner.do") public ModelAndView viewPartner(ModelAndView
+ * mav) { mav.setViewName("partner/partnerList"); List<UserDTO> list =
+ * partnerDAO.getAllUser();
+ * 
+ * mav.addObject("list",list); return mav; } }
+ */
