5efae301a3ae73c00ae2ec7384e2accc816d22fc
zbass <smilejkh90@gmail.com>
2019-04-23 17:30:19 +0900

    hh

--- src/main/java/kr/goott/gymcarry/controller/UserController.java
+++ src/main/java/kr/goott/gymcarry/controller/UserController.java
@@ -1,10 +1,8 @@
 package kr.goott.gymcarry.controller;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.annotation.Resource;
 import javax.inject.Inject;
@@ -13,12 +11,9 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
